1f303506addeff538a51d26ce254bc86eb3b2c7f
HerrDuck <137423953+HerrDuck@users.noreply.github.com>
2024-09-13 12:58:21 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("Hello, world!")
+x = 1
+
+class exit():
+    def run():
+        x = 0
+
+def runCommand(string):
+    string.run()
+
+while x == 1:
+    playerInput = input("> ")
+    runCommand(playerInput)
+    
